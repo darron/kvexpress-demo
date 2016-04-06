e3df24c3738fdeb7800c742505309a3032fd3a8a
Darron Froese <darron@froese.org>
2016-04-06 08:21:35 -0600

    Reconfigure an already installed Datadog agent. Startup goshe to watch dnsmasq stats.

--- terraform/scripts/datadog.sh
+++ terraform/scripts/datadog.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 DD_API_KEY=$(cat /tmp/datadog-api-key)
-export DD_API_KEY=$DD_API_KEY
-bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
-sudo cat > /tmp/consul.yaml <<EOF
-init_config:
-
-instances:
-    - url: http://localhost:8500
-      catalog_checks: yes
-      new_leader_checks: yes
+NODE_NAME=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+# Create a new config file.
+cat > /tmp/datadog.conf <<EOF
+[Main]
+api_key: $DD_API_KEY
+hostname: $NODE_NAME
+dd_url: https://app.datadoghq.com
+gce_updated_hostname: yes
+use_mount: no
 EOF
-cd /tmp
-sudo mv -f /tmp/consul.yaml /etc/dd-agent/conf.d/consul.yaml
-sudo service datadog-agent restart
+sudo mv -f /tmp/datadog.conf /etc/dd-agent/datadog.conf
+
+# Start it up.
+sudo update-rc.d datadog-agent enable
+sudo service datadog-agent restart || true
+
+cat > /tmp/goshednsmasq.conf <<EOF
+description "goshe dnsmasq stats gathering daemon"
+
+# Defaults set by kernel
+limit nofile 1024 4096
+
+emits goshednsmasq-up
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+
+exec /usr/local/bin/goshe dnsmasq
+
+post-start exec initctl emit goshednsmasq-up
+
+kill signal INT
+EOF
+
+sudo mv -f /tmp/goshednsmasq.conf /etc/init/goshednsmasq.conf
+sudo ln -s /lib/init/upstart-job /etc/init.d/goshednsmasq
+sudo service goshednsmasq start
